4c4e8e094dcc6fa34303646ac94e807f2e5ae67e
Shashi Kumar <shashikumar@Shashis-MacBook-Air.local>
2025-08-06 20:31:30 +0530

    episode-01

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
 
 
-
+/*
 const parent = React.createElement(
     "div",
     {id:"parent"}, 
@@ -15,6 +15,37 @@ const parent = React.createElement(
         "div",
         {id:"child"}, 
         React.createElement("h1",{},"I am h1 Tag")));
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+*/
+
+
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    [
+        React.createElement(
+        "div",
+        {id:"child"},
+        
+            React.createElement("h1",{},"i am h1"),
+            React.createElement("h2",{},"i am h2"),
+        ),
+
+        React.createElement(
+            "div",
+            {id:"child2"},
+            [
+            React.createElement("h1",{},"i am h1 tag"),
+            React.createElement("h2",{},"i am h2 tag"),
+            ]
+        ),
+        
+    ]
+);
 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
